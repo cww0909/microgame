dd7c2c34a33be5132aea2159c5136065a60ead28
cww0909 <cww9989@gmail.com>
2014-01-08 03:17:38 +0800

    fixed a bug: changed process.env.port to process.env.PORT

--- web.js
+++ web.js
@@ -4,6 +4,6 @@ var app = express();
 app.use(express.compress());
 app.use(express.static(__dirname+"/public",{index: "game.html"}));
 
-var port = process.env.port || 8888;
+var port = process.env.PORT || 8888;
 app.listen(port);
 console.log("Server ready to serve game on port "+port);
